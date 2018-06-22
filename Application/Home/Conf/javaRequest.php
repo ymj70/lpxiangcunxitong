499bc8b6476f89655da70fecd408ef19e7f367a1
wyd <15033307336@163.com>
2018-06-22 09:40:28 +0800

    添加医疗保险登记控制器

--- Application/Home/Conf/javaRequest.php
+++ Application/Home/Conf/javaRequest.php
@@ -2,9 +2,18 @@
 return array(
     //程序中访问java接口的映射 当访问java接口时获取该配置中的对应url去访问 而不是在程序中把接口url写死 这样便于修改
     "javaUrl" => array(
-        "Public"=>array(
-            "login"=>"/user/login",
-        )
+        "Public" => array(
+            "login" => "/user/login",//登录
+        ),
+        "NotAttendIns" => array(
+            "checkPeopleInsStatus"=>"",//检测是否参保
+            "savePeopleInfo"=>"",//将未参保人员信息存入数据库
+        ),
+        "MedicalInsAttend" => array(
+            "getPeopleInfo"=>"",//根据身份证号获取信息
+            "checkPeopleMedicalInsStatus"=>"",//根据身份证号检测是否参加城乡居民医疗和城镇职工医疗
+            "savePeopleInfo"=>"",//将参加城乡医疗保险的的人员信息保存到数据路
+        ),
     ),
 );
 
