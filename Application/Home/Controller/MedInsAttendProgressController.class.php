c3c604707fd4896b7db15a8ee9dfacda68399378
wyd <15033307336@163.com>
2018-06-26 15:36:03 +0800

    医疗参保 养老参保

---
+++ Application/Home/Controller/MedInsAttendProgressController.class.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Home\Controller;
+class MedInsAttendProgressController extends CommonController
+{
+    public function _initialize()
+    {
+        parent::_initialize();
+        //为菜单选中状态设置session
+        session("menu", "养老参保进度查询");
+        session("model", "信息查询");
+    }
+
+    /**
+     *城乡医疗参保登记首页
+     */
+    public function attentionMatters(){
+        $this->display("attentionMatters");
+    }
+    public function getIdcardInfo(){
+        $this->display("getIdcardInfo");
+    }
+    public function showResult(){
+        $this->display("showResult");
+    }
+}
